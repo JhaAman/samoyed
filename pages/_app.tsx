238c2f8c2907d1de4177ebcace0b0de96e09b701
Aman Jha <gunujha@gmail.com>
2021-12-31 09:17:03 -0800

    adding themes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,14 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import UserProvider from "../utils/user";
 import Nav from "../components/Nav";
+import { ThemeProvider } from "next-themes";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <UserProvider>
-      <Nav />
-      <Component {...pageProps} />
+      <ThemeProvider>
+        <Component {...pageProps} />
+      </ThemeProvider>
     </UserProvider>
   );
 }
