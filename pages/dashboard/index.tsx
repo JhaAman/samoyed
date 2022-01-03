ee2dcb9ed35d2d0c611c90191c3143e54b80da48
Aman Jha <gunujha@gmail.com>
2022-01-03 11:33:16 -0800

    added trunk

--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -1,12 +1,48 @@
+import { useRouter } from "next/router";
 import { useEffect } from "react";
+import supabase from "../../utils/supabase";
 import { useUser } from "../../utils/user";
 
-const Dashboard = () => {
-  const { logout } = useUser();
+interface Props {
+  beta_list: {
+    id: number;
+    name: string;
+    email: string;
+  }[];
+}
 
-  useEffect(() => {});
+const Dashboard = ({ beta_list }: Props) => {
+  const router = useRouter();
+  const { user, profile, logout } = useUser();
+
+  useEffect(() => {
+    if (!user) {
+      router.push("/dashboard");
+    } else {
+      // Check if user is in beta list
+      const isInBetaList = beta_list.some(
+        (beta_list_user) => beta_list_user.email === user.email
+      );
+      if (!isInBetaList) {
+        console.log("User is not in beta list");
+      }
+
+      // TODO: Check if user is subscribed, has lifetime access, or is on a trial
+    }
+  }, [beta_list, router, user]);
 
   return <p>Welcome to dashboard</p>;
+};
+
+export const getStaticProps = async () => {
+  const { data: beta_list, error } = await supabase
+    .from("beta_list")
+    .select("*");
+  return {
+    props: {
+      beta_list,
+    },
+  };
 };
 
 export default Dashboard;
