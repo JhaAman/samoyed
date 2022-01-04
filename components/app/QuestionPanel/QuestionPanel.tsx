4e296efac4274b54649bed8d64ae4115b7669d3b
Aman Jha <gunujha@gmail.com>
2022-01-04 15:00:51 -0800

    skipping tags for now

--- components/app/QuestionPanel/QuestionPanel.tsx
+++ components/app/QuestionPanel/QuestionPanel.tsx
@@ -3,16 +3,39 @@ import * as Separator from "@radix-ui/react-separator";
 import Button from "../../ui/Button";
 import { motion, AnimateSharedLayout } from "framer-motion";
 import Slider from "./Slider";
+import Tags from "./Tags";
 
 interface Props {}
 
 export default function QuestionPanel({}: Props): ReactElement {
   return (
+    // Card bg
     <div className="m-10 rounded h-1/2 bg-surface">
-      <div className="w-1/3 px-5 py-5 m-4">
-        <h1 className="text-2xl">Question</h1>
+      <div className="flex m-5">
+        {/* Left */}
+        <div className="flex flex-col justify-between h-full">
+          <h1 className="text-lg">Question</h1>
+
+          {/* Explain variable */}
+          <div className="flex flex-col justify-between mt-8 ">
+            <h1 className="mb-2 text-sm">What would you like more of?</h1>
+            <div>
+              <Slider />
+              <div className="flex justify-between">
+                <h3 className="text-xs">Explain</h3>
+                <h3 className="text-xs">Code</h3>
+              </div>
+            </div>
+          </div>
+
+          {/* Tags */}
+          {/* <div className="max-w-10">
+            <Tags />
+          </div> */}
+        </div>
+
+        {/* Right */}
       </div>
-      <Slider />
     </div>
   );
 }
