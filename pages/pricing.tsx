a1145f5a31f5c17841ede361792eea86fc81ceb5
Aman Jha <gunujha@gmail.com>
2022-01-03 16:03:18 -0800

    cancel pricing work for now

--- pages/pricing.tsx
+++ pages/pricing.tsx
@@ -10,30 +10,30 @@ const Pricing = ({ plans }: Props) => {
   return <div></div>;
 };
 
-export const getStaticProps = async () => {
-  const stripe = new initStripe(
-    process.env.STRIPE_SECRET_KEY as string,
-    {} as any
-  );
+// export const getStaticProps = async () => {
+//   const stripe = new initStripe(
+//     process.env.STRIPE_SECRET_KEY as string,
+//     {} as any
+//   );
 
-  const { data: prices } = await stripe.prices.list();
-  // for each price in prices, retrieve the product associated with the price
-  const plans = await Promise.all(
-    prices.map(async (price: any) => {
-      const product = await stripe.products.retrieve(price.product);
-      return {
-        ...price,
-        product,
-      };
-    })
-  );
-  const sortedPlans = plans.sort((a, b) => a.unit_amount - b.unit_amount);
+//   const { data: prices } = await stripe.prices.list();
+//   // for each price in prices, retrieve the product associated with the price
+//   const plans = await Promise.all(
+//     prices.map(async (price: any) => {
+//       const product = await stripe.products.retrieve(price.product);
+//       return {
+//         ...price,
+//         product,
+//       };
+//     })
+//   );
+//   const sortedPlans = plans.sort((a, b) => a.unit_amount - b.unit_amount);
 
-  return {
-    props: {
-      sortedPlans,
-    },
-  };
-};
+//   return {
+//     props: {
+//       sortedPlans,
+//     },
+//   };
+// };
 
 export default Pricing;
