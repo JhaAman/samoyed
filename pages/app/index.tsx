e6e9be22f2810c53e283a575db379456cf68ca26
Aman Jha <gunujha@gmail.com>
2022-01-03 21:21:49 -0800

    history view

--- pages/app/index.tsx
+++ pages/app/index.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import { useRouter } from "next/router";
 import React, { useState } from "react";
 import { useEffect } from "react";
@@ -39,7 +40,42 @@ const App = ({ beta_list }: Props) => {
   return (
     <>
       {/* Main App */}
-      <div className="h-screen "></div>
+      <div className="flex h-screen">
+        {/* Left Side */}
+        <div className="relative flex flex-col justify-between w-1/3 h-screen bg-base">
+          {/* History */}
+          <div className="flex-1 w-full overflow-y-scroll">
+            {/* Have 10 divs mapped */}
+            {Array.from({ length: 10 }).map((_, index) => (
+              <div key={index} className="flex px-4 py-3 hover:bg-surface">
+                <div>
+                  <p className="flex items-baseline">
+                    <span className="mr-2 text-sm font-medium text-green-500">
+                      Aman Jha
+                    </span>
+                    <span className="text-xs text-gray-500">Dec 30, 2021</span>
+                  </p>
+                  <p className="text-gray-300 opacity-70">
+                    Tailwind comes with pre-built, single-purpose CSS classes
+                    called utility classes that you use to style your
+                    application.
+                  </p>
+                </div>
+              </div>
+            ))}
+          </div>
+          {/* New Question Button */}
+          <div className="flex items-center justify-center w-full h-20 ">
+            <button className="w-full h-full mx-2 text-2xl uppercase border-2 border-primary">
+              New Question
+            </button>
+          </div>
+        </div>
+        {/* Right Side */}
+        <div className="flex flex-col w-2/3 h-screen border-l-2 bg-base border-l-white">
+          Chat
+        </div>
+      </div>
     </>
   );
 };
