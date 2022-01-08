749cf1f837d71362d73a6941ee1cb3c877e3be67
Aman Jha <gunujha@gmail.com>
2022-01-08 15:24:17 -0800

    type input box

--- components/app/AnswerPanel/AnswerPanel.tsx
+++ components/app/AnswerPanel/AnswerPanel.tsx
@@ -11,8 +11,8 @@ interface Props {
 export default function AnswerPanel({ answer }: Props): ReactElement {
   return (
     // Card bg
-    <div className="flex-1 mx-10 mb-10 overflow-y-auto rounded h-fit bg-surface">
-      <div className="flex justify-between mx-16 my-8 whitespace-pre-wrap">
+    <div className="flex-1 mx-10 mb-10 overflow-y-auto rounded bg-surface">
+      <div className="flex justify-between h-full py-8 mx-16 whitespace-pre-wrap">
         {/* Left */}
         <div className="flex flex-col w-1/2 h-full ">
           <div className="flex-grow p-4 rounded bg-overlay-dark">
@@ -23,10 +23,8 @@ export default function AnswerPanel({ answer }: Props): ReactElement {
         </div>
 
         {/* Right */}
-        <div className="flex flex-col h-full p-4 ml-8 rounded bg-overlay-dark">
-          <div className="flex flex-col ">
-            <Clarify />
-          </div>
+        <div className="flex flex-col flex-1 p-4 ml-8 rounded bg-overlay-dark">
+          <Clarify />
         </div>
       </div>
     </div>
--- components/app/AnswerPanel/Clarify/Clarify.tsx
+++ components/app/AnswerPanel/Clarify/Clarify.tsx
@@ -1,8 +1,38 @@
 import React, { ReactElement } from "react";
 import Chatbot from "react-chatbot-kit";
+import { Input } from "@supabase/ui";
+import { CheckIcon, SelectorIcon } from "@heroicons/react/solid";
 
 interface Props {}
 
 export default function Clarify({}: Props): ReactElement {
-  return <div className="">jha</div>;
+  return (
+    <div className="relative flex flex-col justify-between h-full">
+      <div className="flex-1 w-full overflow-y-scroll">
+        <p>Ask a clarifying question</p>
+        <p>Ask a clarifying question</p>
+        <p>Ask a clarifying question</p>
+        <p>Ask a clarifying question</p>
+        <p>Ask a clarifying question</p>
+        <p>Ask a clarifying question</p>
+        <p>Ask a clarifying question</p>
+        <p>Ask a clarifying question</p>
+        <p>Ask a clarifying question</p>
+        <p>Ask a clarifying question</p>
+        <p>Ask a clarifying question</p>
+        <p>Ask a clarifying question</p>
+        <p>Ask a clarifying question</p>
+        <p>Ask a clarifying question</p>
+        <p>Ask a clarifying question</p>
+        <p>Ask a clarifying question</p>
+        <p>Ask a clarifying question</p>
+      </div>
+      <div className="flex w-full mt-5 h-fit">
+        <Input className="w-full" placeholder="Type here"></Input>
+        <span className="flex items-center px-2 pointer-events-none ">
+          <SelectorIcon className="w-5 h-5 text-gray-400" aria-hidden="true" />
+        </span>
+      </div>
+    </div>
+  );
 }
