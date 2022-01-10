59c1be65c517336a328ff722d91e1860274140f2
Aman Jha <gunujha@gmail.com>
2022-01-10 14:25:42 -0800

    google example pic

--- pages/index.tsx
+++ pages/index.tsx
@@ -46,6 +46,107 @@ const Home = ({ beta_list }: Props) => {
     <>
       {state === 0 && (
         <div className="flex items-center justify-between min-h-screen text-white bg-gradient-to-tr from-hero-left to-hero-right">
+          <div className="flex flex-col w-2/5 ml-20 ">
+            <div>
+              <h1 className="animate-[fadeIn_0.5s_linear] my-4 text-5xl">
+                As Easy As Coding Gets
+              </h1>
+              <h2 className="animate-[fadeIn_0.5s_linear] leading-relaxed max-w-prose">
+                We{"'"}re tired of crawling through StackOverflow and done with
+                tutorial hell.
+                <br />
+                So we built{" "}
+                <span className="font-semibold text-primary">Rosie</span>
+              </h2>
+            </div>
+            <div className="flex items-center justify-center w-full mt-8">
+              <button
+                onClick={() =>
+                  setState((prevState) => Math.min(prevState + 1, 3))
+                }
+                className="animate-[fadeIn_0.5s_linear] flex items-center mt-4 py-2 text-sm font-medium text-white transition transform rounded-full backface-visibility-hidden active:bg-opacity-40 hover:scale-105 hover:bg-opacity-30 focus:outline-none bg-opacity-20"
+              >
+                <span className="">
+                  Press{" "}
+                  <span className="px-1 py-0.5 rounded m-1 font-bold bg-gray-800">
+                    enter
+                  </span>{" "}
+                  to begin <span className="w-5 h-5 opacity-70">&#8594;</span>
+                </span>
+              </button>
+            </div>
+          </div>
+
+          <div className="pt-1 pl-1 rounded-l-xl bg-primary animate-[fadeIn_1s_linear]">
+            <Image
+              src="/ss.png"
+              alt="Rosie"
+              width={653}
+              height={500}
+              className="rounded-l-xl"
+            />
+          </div>
+        </div>
+      )}
+      {state === 1 && (
+        <div className="flex items-center justify-between min-h-screen text-white bg-gradient-to-tr from-hero-left to-hero-right">
+          <div className="flex flex-col w-2/5 ml-20 left">
+            <div>
+              <h2 className="animate-[fadeIn_0.5s_linear] text-xl leading-relaxed max-w-prose">
+                You spend 50% of your time debugging and learning new things.
+                <br />
+                <br />
+                What if you didn{"'"}t have to go through dozens of links and
+                videos to find the answer you want?
+              </h2>
+            </div>
+            <div className="flex flex-col items-center justify-center w-full mt-8">
+              <button
+                onClick={() =>
+                  setState((prevState) => Math.min(prevState + 1, 3))
+                }
+                className="animate-[fadeIn_0.5s_linear] flex items-center mt-4 py-2 text-sm font-medium text-white transition transform rounded-full backface-visibility-hidden active:bg-opacity-40 hover:scale-105 hover:bg-opacity-30 focus:outline-none bg-opacity-20"
+              >
+                <span className="">
+                  Press{" "}
+                  <span className="px-1 py-0.5 rounded m-1 font-bold bg-gray-800">
+                    enter
+                  </span>{" "}
+                  to continue{" "}
+                  <span className="w-5 h-5 opacity-70">&#8594;</span>
+                </span>
+              </button>
+
+              <button
+                onClick={() =>
+                  setState((prevState) => Math.min(prevState - 1, 0))
+                }
+                className="animate-[fadeIn_0.5s_linear] flex items-center py-1 text-sm font-medium text-white transition transform rounded-full backface-visibility-hidden active:bg-opacity-40 hover:scale-105 hover:bg-opacity-30 focus:outline-none bg-opacity-20"
+              >
+                <span className="">
+                  <span className="w-5 h-5 opacity-70">&#8592;</span>
+                  <span className="px-1 py-0.5 rounded m-1 font-bold bg-gray-800">
+                    backspace
+                  </span>{" "}
+                  to go back
+                </span>
+              </button>
+            </div>
+          </div>
+
+          <div className="mr-20 border rounded-xl border-primary">
+            <Image
+              src="/google_example.png"
+              alt="Rosie"
+              className=" rounded-xl border-primary"
+              width={500}
+              height={550}
+            />
+          </div>
+        </div>
+      )}
+      {/* {state === 2 && (
+        <div className="flex items-center justify-between min-h-screen text-white bg-gradient-to-tr from-hero-left to-hero-right">
           <div className="flex flex-col w-2/5 ml-20 left">
             <div>
               <h1 className="animate-[fadeIn_0.5s_linear] my-4 text-5xl">
@@ -81,103 +182,45 @@ const Home = ({ beta_list }: Props) => {
             <Image src="/ss.png" alt="Rosie" width={653} height={500} />
           </div>
         </div>
-      )}
-      {state === 1 && (
+      )} */}
+      {/* {state === 3 && (
         <div className="flex items-center justify-between min-h-screen text-white bg-gradient-to-tr from-hero-left to-hero-right">
-          <div className="w-1/2 ml-20 left">
-            <h1 className="animate-[fadeIn_0.5s_linear] my-4 text-5xl">
-              press
-            </h1>
-            <h2 className="animate-[fadeIn_0.5s_linear] leading-relaxed max-w-prose">
-              We{"'"}re tired of crawling through StackOverflow and done with
-              tutorial hell.
-              <br />
-              So we built{" "}
-              <span className="font-semibold text-primary">Rosie</span>
-            </h2>
-            <button
-              onClick={() =>
-                setState((prevState) => Math.min(prevState + 1, 3))
-              }
-              className="animate-[fadeIn_0.5s_linear] flex items-center mt-4 py-2 text-sm font-medium text-white transition transform rounded-full backface-visibility-hidden active:bg-opacity-40 hover:scale-105 hover:bg-opacity-30 focus:outline-none bg-opacity-20"
-            >
-              <span className="">
-                Press{" "}
-                <span className="px-1 py-0.5 rounded m-1 font-bold bg-gray-800">
-                  enter
-                </span>{" "}
-                to begin <span className="w-5 h-5 opacity-70">&#8594;</span>
-              </span>
-            </button>
+          <div className="flex flex-col w-2/5 ml-20 left">
+            <div>
+              <h1 className="animate-[fadeIn_0.5s_linear] my-4 text-5xl">
+                As Easy As Coding Gets
+              </h1>
+              <h2 className="animate-[fadeIn_0.5s_linear] leading-relaxed max-w-prose">
+                We{"'"}re tired of crawling through StackOverflow and done with
+                tutorial hell.
+                <br />
+                So we built{" "}
+                <span className="font-semibold text-primary">Rosie</span>
+              </h2>
+            </div>
+            <div className="flex items-center justify-center w-full mt-8">
+              <button
+                onClick={() =>
+                  setState((prevState) => Math.min(prevState + 1, 3))
+                }
+                className="animate-[fadeIn_0.5s_linear] flex items-center mt-4 py-2 text-sm font-medium text-white transition transform rounded-full backface-visibility-hidden active:bg-opacity-40 hover:scale-105 hover:bg-opacity-30 focus:outline-none bg-opacity-20"
+              >
+                <span className="">
+                  Press{" "}
+                  <span className="px-1 py-0.5 rounded m-1 font-bold bg-gray-800">
+                    enter
+                  </span>{" "}
+                  to begin <span className="w-5 h-5 opacity-70">&#8594;</span>
+                </span>
+              </button>
+            </div>
           </div>
 
           <div className=" right">
             <Image src="/ss.png" alt="Rosie" width={653} height={500} />
           </div>
         </div>
-      )}
-      {state === 2 && (
-        <div className="flex items-center justify-between min-h-screen text-white bg-gradient-to-tr from-hero-left to-hero-right">
-          <div className="w-1/2 ml-20 left">
-            <h1 className="animate-[fadeIn_0.5s_linear] my-4 text-5xl">demo</h1>
-            <h2 className="animate-[fadeIn_0.5s_linear] leading-relaxed max-w-prose">
-              We{"'"}re tired of crawling through StackOverflow and done with
-              tutorial hell.
-              <br />
-              So we built{" "}
-              <span className="font-semibold text-primary">Rosie</span>
-            </h2>
-            <button
-              onClick={() => {}}
-              className="animate-[fadeIn_0.5s_linear] flex items-center mt-4 py-2 text-sm font-medium text-white transition transform rounded-full backface-visibility-hidden active:bg-opacity-40 hover:scale-105 hover:bg-opacity-30 focus:outline-none bg-opacity-20"
-            >
-              <span className="">
-                Press{" "}
-                <span className="px-1 py-0.5 rounded m-1 font-bold bg-gray-800">
-                  enter
-                </span>{" "}
-                to begin <span className="w-5 h-5 opacity-70">&#8594;</span>
-              </span>
-            </button>
-          </div>
-
-          <div className=" right">
-            <Image src="/ss.png" alt="Rosie" width={653} height={500} />
-          </div>
-        </div>
-      )}
-      {state === 3 && (
-        <div className="flex items-center justify-between min-h-screen text-white bg-gradient-to-tr from-hero-left to-hero-right">
-          <div className="w-1/2 ml-20 left">
-            <h1 className="animate-[fadeIn_0.5s_linear] my-4 text-5xl">
-              preorder
-            </h1>
-            <h2 className="animate-[fadeIn_0.5s_linear] leading-relaxed max-w-prose">
-              We{"'"}re tired of crawling through StackOverflow and done with
-              tutorial hell.
-              <br />
-              So we built{" "}
-              <span className="font-semibold text-primary">Rosie</span>
-            </h2>
-            <button
-              onClick={() => {}}
-              className="animate-[fadeIn_0.5s_linear] flex items-center mt-4 py-2 text-sm font-medium text-white transition transform rounded-full backface-visibility-hidden active:bg-opacity-40 hover:scale-105 hover:bg-opacity-30 focus:outline-none bg-opacity-20"
-            >
-              <span className="">
-                Press{" "}
-                <span className="px-1 py-0.5 rounded m-1 font-bold bg-gray-800">
-                  enter
-                </span>{" "}
-                to begin <span className="w-5 h-5 opacity-70">&#8594;</span>
-              </span>
-            </button>
-          </div>
-
-          <div className=" right">
-            <Image src="/ss.png" alt="Rosie" width={653} height={500} />
-          </div>
-        </div>
-      )}
+      )} */}
     </>
   );
 };
